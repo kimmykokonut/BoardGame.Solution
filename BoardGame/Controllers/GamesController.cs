6041889d4007012dfbab7a400d928eda76ad0fee
Kim Robinson <kimmykokonut@gmail.com>
2024-01-30 10:49:53 -0800

    add functionality to view game details
    Co-authored-by: Noah Kise <noah@kisefamily.com>

--- BoardGame/Controllers/GamesController.cs
+++ BoardGame/Controllers/GamesController.cs
@@ -19,5 +19,23 @@ namespace BoardGame.Controllers
       List<Game> model = _db.Games.ToList();
       return View(model);
     }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Game game)
+    {
+      _db.Games.Add(game);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+    public ActionResult Details(int id)
+    {
+      Game thisGame = _db.Games.FirstOrDefault(game => game.GameId == id);
+      return View(thisGame);
+    }
   }
 }
